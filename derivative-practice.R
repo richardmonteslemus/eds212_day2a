688ec8234bf696e6ba1b491b5e35959842604b65
richardmonteslemus <richard.montes.lemus@gmail.com>
2025-08-05 12:04:20 -0700

    Initial commit

---
+++ derivative-practice.R
@@ -0,0 +1,50 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                            Derivative practice                           ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#................Practice using the function D().................
+
+# create an expression 
+my_expression <- expression(5 * x^2)
+
+# find derivative of the expression with respect to x 
+my_derivative <- D(expr = my_expression, name = "x")
+
+# evaluate derivative at x = 2.8 
+x <- 2.8 
+# evaluate! 
+
+eval(my_derivative)
+
+#....................another example with D()....................
+# find derivative with respect to x 
+my_derivative <- D(expr = expression(3.1 * x^4 - 28 * x), name = "x")
+my_derivative
+
+# create and store a function 
+fx <- expression(x^2)
+
+# find the derivative with respect to x 
+df_dx <- D(expr = fx, name = "x")
+df_dx 
+
+# My turn 
+
+my_derivative2 <- D(expr = expression(2*z^3 - 10.5*z^2 + 4.1), name = "z")
+my_derivative2
+
+
+dt_dy <- D(expr = expression((2*y^3 + 1)^4 - 8*y^3), name = "y")
+dt_dy
+
+#example 
+gz = expression(2*z^3 - 10.5 * z^2 +4.1)
+dg_gz <- D(expr = gz, name = "z")
+
+
+# find the slope of T(y) at a range of values
+y <- seq(from = -0.4, to = 2.0, by = 0.1) 
+
+#evaluate the slope of T(y) at each of those values 
+eval(dt_dy)
